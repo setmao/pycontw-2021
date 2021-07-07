c8aa394788285288c72740b9654495692ec8fadc
Matt.Wang <mattwang44@gmail.com>
2021-07-08 02:39:56 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
 module.exports = {
     mode: 'jit',
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                ...fontFamily,
+                sans: [
+                    'Source Sans Pro',
+                    '-apple-system',
+                    'Roboto',
+                    'Helvetica Neue',
+                    'sans-serif',
+                ],
+                serif: ['Noto Serif TC', '-apple-system', 'serif'],
+            },
+        },
     },
     variants: {
         extend: {},
